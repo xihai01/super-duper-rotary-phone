53c4fb8eb52462576b06e49b796dca9d748ad6b1
Xihai Luo <xihai.luo01@gmail.com>
2022-07-01 17:53:32 -0400

    implement /repo route with axios to fetch SO repo data from github API

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,25 @@
 import { Router, Request, Response } from 'express';
+import Axios from 'axios';
+import { Repo } from '../models/Repo';
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
+  res.header('Content-Type', 'application/json');
+
+  let response: Repo[] = [];
+  try {
+    const apiResponse = await Axios.get(
+      'https://api.github.com/users/silverorange/repos'
+    );
+    response = apiResponse.data;
+  } catch (error) {
+    response = [];
+  }
 
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  res.json(response);
 });
