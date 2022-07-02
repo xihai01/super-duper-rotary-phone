13a22a0bdf241855df0af402846c7484a0ca8958
Xihai Luo <xihai.luo01@gmail.com>
2022-07-02 10:16:47 -0400

    add button features to allow filtering list of repos by languages

--- web/src/components/Home.tsx
+++ web/src/components/Home.tsx
@@ -1,9 +1,42 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import axios from 'axios';
 
 export function Home() {
   // use state goes here
   const [repo, setRepo] = useState<any[]>([]);
+  const [listOfRepo, setListOfRepo] = useState<any[]>([]);
+  const [filter, setFilter] = useState<{ filterBy: string }>({
+    filterBy: 'All',
+  });
+
+  // handle repo list filtering by language
+  const handleClick = (filterWord: string) => {
+    setFilter({ filterBy: filterWord });
+  };
+
+  const showListOfRepo = useCallback(
+    (filterObj: { filterBy: string }) => {
+      const { filterBy } = filterObj;
+      const repositories: any[] = [];
+      repo.forEach((data) => {
+        if (data.language === filterBy || filterBy === 'All') {
+          repositories.push(
+            <article key={data.id}>
+              <h2>{data.name}</h2>
+              <ul>
+                <li>{data.created_at}</li>
+                <li>{data.description ? data.description : 'no desc'}</li>
+                <li>{data.language ? data.language : 'no language'}</li>
+                <li>{data.forks_count ? data.forks_count : 'unavilable'}</li>
+              </ul>
+            </article>
+          );
+        }
+      });
+      return repositories;
+    },
+    [repo]
+  );
 
   // use efffect goes here
   useEffect(() => {
@@ -18,18 +51,19 @@ export function Home() {
     };
     fetchRepo();
   }, []);
-  const listOfRepo = repo.map((data) => {
-    return (
-      <article key={data.id}>
-        <h2>{data.name}</h2>
-        <ul>
-          <li>{data.created_at}</li>
-          <li>{data.description ? data.description : 'no desc'}</li>
-          <li>{data.language ? data.language : 'no language'}</li>
-          <li>{data.forks_count ? data.forks_count : 'unavilable'}</li>
-        </ul>
-      </article>
-    );
-  });
-  return <div>{repo.length === 0 ? 'Loading' : listOfRepo}</div>;
+
+  useEffect(() => {
+    setListOfRepo(showListOfRepo(filter));
+  }, [filter, showListOfRepo]);
+
+  return (
+    <div>
+      <button onClick={() => handleClick('All')}>All</button>
+      <button onClick={() => handleClick('PHP')}>PHP</button>
+      <button onClick={() => handleClick('TypeScript')}>TypeScript</button>
+      <button onClick={() => handleClick('English')}>English</button>
+      <button onClick={() => handleClick('French')}>French</button>
+      <section> {repo.length === 0 ? 'Loading' : listOfRepo} </section>
+    </div>
+  );
 }
