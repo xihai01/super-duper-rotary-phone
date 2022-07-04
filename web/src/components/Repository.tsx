6982e61297938b12c3f05c537e45ca7fc620903f
Xihai Luo <xihai.luo01@gmail.com>
2022-07-04 00:14:55 -0400

    add functionailty in repository route to render markdown content if available

--- web/src/components/Repository.tsx
+++ web/src/components/Repository.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
 import axios from 'axios';
 
 export function Repository() {
   const { name } = useParams();
   // use state goes here
   const [commits, setCommits] = useState<any[]>([]);
+  const [readMe, setReadMe] = useState<string>('');
 
   // use efffect goes here
   useEffect(() => {
@@ -14,9 +16,14 @@ export function Repository() {
         const { data } = await axios.get(
           `https://api.github.com/repos/silverorange/${name}/commits`
         );
+        const readMeData = await axios.get(
+          `https://raw.githubusercontent.com/silverorange/${name}/master/README.md`
+        );
         setCommits(data);
+        setReadMe(readMeData.data);
       } catch (error) {
         setCommits([]);
+        setReadMe('');
       }
     };
     fetchCommits();
@@ -27,13 +34,30 @@ export function Repository() {
       <h1>Most Recent Commit</h1>
       <ul>
         <li>
-          {commits.length === 0 ? 'Loading' : commits[0].commit.author.name}
+          {commits.length === 0
+            ? 'No name available'
+            : commits[0].commit.author.name}
         </li>
         <li>
-          {commits.length === 0 ? 'Loading' : commits[0].commit.author.date}
+          {commits.length === 0
+            ? 'No author available'
+            : commits[0].commit.author.date}
         </li>
-        <li>{commits.length === 0 ? 'Loading' : commits[0].commit.message}</li>
+        <li>
+          {commits.length === 0
+            ? 'No commit message available'
+            : commits[0].commit.message}
+        </li>
       </ul>
+      <br />
+      <h1>README.md</h1>
+      <section>
+        {readMe.length === 0 ? (
+          'No README.md available'
+        ) : (
+          <ReactMarkdown>{readMe}</ReactMarkdown>
+        )}
+      </section>
       <Link to="/">Back Home</Link>
     </div>
   );
